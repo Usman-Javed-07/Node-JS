6dc3c3a7db615d5b4ff7351d056672e29b4145a3
Usman Javed <usmanjaved0816@gmail.com>
2024-11-05 17:45:26 +0500

    http get method in node js

--- app.js
+++ app.js
@@ -2,16 +2,24 @@
 
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+const logger = require('./logger')
+const authorize = require('./authorize')
 
-// middleware 
-const logger = (req , res, next)=> {
-    const method = req.method;
-    const url = req.url
-    const time = new Date().getFullYear()
-    console.log(method , url , time);
-    next()
-    
-}
+
+// 
+// 1. use vs route
+// 2. option -  our own / express / third party
+
+
+// app.use([logger, authorize])
+// app.use(express.static('./public'))
+
+app.use(morgan('tiny'))
+app.get('/', (req , res)=> {
+    res.send('Home')
+})
+
 
 app.get('/' , logger , (req, res)=> {
     res.send('Home')
@@ -19,8 +27,12 @@ app.get('/' , logger , (req, res)=> {
 app.get('/about' ,logger, (req , res)=> {
     res.send('about')
 })
-
-
+app.get('/api/products' ,logger, (req , res)=> {
+    res.send('products')
+})
+app.get('/api/items' , (req , res)=> {
+    res.send('items')
+})
 
 app.listen(5000, (req , res)=> {
     console.log('server is listing on port 5000...');
