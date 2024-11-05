75c150b653602ac77fb48454ac10029bcbfbd087
Usman Javed <usmanjaved0816@gmail.com>
2024-11-05 16:53:37 +0500

    middleware in node js

--- app.js
+++ app.js
@@ -2,24 +2,25 @@
 
 const express = require('express');
 const app = express();
-const {products} = require('./data')
 
-app.get('/', (req, res)=> {
-    res.send('<h1> Home page</h1> <a href = "/api/products"> producs </a>')
-})
-app.get('/api/products', (req, res) => {
-    const newProduct = products.map((product)=> {
-        const {id, name , image} = product
-        return {id, name , image}
-    })
-    res.json(newProduct)
-})
-app.get('/api/products/:productID', (req, res) => {
-    const { productID } = req.params;
-    const singleProduct = products.find((product)=>  product.id === Number(productID))
+// middleware 
+const logger = (req , res, next)=> {
+    const method = req.method;
+    const url = req.url
+    const time = new Date().getFullYear()
+    console.log(method , url , time);
+    next()
     
- return  res.json(singleProduct)
+}
+
+app.get('/' , logger , (req, res)=> {
+    res.send('Home')
 })
+app.get('/about' ,logger, (req , res)=> {
+    res.send('about')
+})
+
+
 
 app.listen(5000, (req , res)=> {
     console.log('server is listing on port 5000...');
