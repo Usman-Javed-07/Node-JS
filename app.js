d26f00ed7a056578f72b1b44804823c7c8f09e17
Usman Javed <usmanjaved0816@gmail.com>
2024-11-06 12:07:50 +0500

    put method in express js

--- app.js
+++ app.js
@@ -6,10 +6,29 @@ let  {people} = require('./data')
 // static assests
 app.use(express.static('./method-public'))
 // parse form data 
-app.use(express.urlencoded({extended: false}))
+// app.use(express.urlencoded({extended: false}))
+app.use(express.json())
 app.get('/api/people', (req, res) => {
     res.status(200).json({success: true , data:people })
 })
+
+app.post('/api/people', (req , res)=> {
+    const {name} = req.body
+    if(!name) {
+        return res.status(400).json({success:false,msg: 'please provide name value'})
+    }
+    res.status(201).send({success: true, person: name})
+})
+
+
+app.post('/api/postman/people', (req , res)=> {
+    const { name } = req.body
+    if(!name) {
+        return res.status(400).json({success: false, msg: 'please provide name value'})
+    }
+    res.status(201).send({success: true, data: [...people, name]})
+})
+
 
 app.post('/login' , (req , res)=>{
     const {name} = req.body;
@@ -17,6 +36,26 @@ app.post('/login' , (req , res)=>{
         return res.status(200).send(`welcome ${name}`)
     }
     res.status(401).send('please provide credentials')
+})
+
+//  put request
+
+app.put ('/api/people/:id', (req , res)=> {
+    const { id } = req.params
+    const { name } = req.body
+    const person = people.find((person)=> person.id === Number(id))
+    if(!person) {
+        return res
+        .status (404)
+        .json({success: false, msg: `no person with id ${id}`})
+    }
+    const newPeople = people.map((person)=> {
+        if(person.id === Number(id)) {
+            person.name = name
+        }
+        return person
+    })
+    res.status(200).json ({success:true, data:newPeople})
 })
 
 app.listen(5000, (req , res)=> {
